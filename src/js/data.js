a1b76af7d0749ce71241b3f9b4212ee7082f0c5d
maochengjie <maochengjie1988@gmail.com>
2012-09-04 15:43:18 +0800

    add static files

---
+++ src/js/data.js
@@ -0,0 +1,35 @@
+// 提供数据层的封装
+
+JJ.Data = function(){
+	
+	var index = [];
+	
+	//返回[start, end]区间包含的JSON数据
+	function getData(start, end){
+	
+	}
+	
+	//初始化，请求数据并保存
+	function init(){
+		baidu.sio.callByServer('http://image.baidu.com/i?tn=resultjson&ct=201326592&cl=2&fm=detail&lm=-1&st=-1&sf=2&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&word=mm&rn=60', 'JJ.Data.dataBack');
+	}
+	
+	//JSONP返回数据时的回调函数
+	function dataBack(data){
+		var arr = data.data, i, len;
+		for(i = 0, len = arr.length; i < len; i++){
+			index[i] = arr[i];
+			index[i].originWidth = arr[i].width;
+			index[i].originHeight = arr[i].height;
+			delete index[i].width;
+			delete index[i].height;
+		}
+	}
+	
+	return {
+		getData : getData, 
+		index : index, 
+		init : init, 
+		dataBack : dataBack
+	};
+}();
