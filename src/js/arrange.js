58211964c2331804768d1a52b1ecd42c07190c30
maochengjie <maochengjie1988@gmail.com>
2012-09-05 18:11:04 +0800

    add pin-packer js

--- src/js/arrange.js
+++ src/js/arrange.js
@@ -4,6 +4,7 @@ JJ.Arrange = function(){
 	var verticalImages = [], 
 		horizontalImages = [], 
 		squareImages = [], 
+		lists = [], 
 		CON_WIDTH = 0, /*容器的宽度*/
 		CON_HEIGHT = 0, /*容器的高度*/
 		CON_SQUARE = 0/*大区域面积*/, 
@@ -25,6 +26,8 @@ JJ.Arrange = function(){
 	
 	//开始计算图片排列
 	function start(){
+	
+///////////////////////////////////////////////////
 		var currArea = 0, 
 			i = 0, len = data.length;
 		imageNum = 0;
@@ -33,6 +36,8 @@ JJ.Arrange = function(){
 		smallArea = smallW * smallH;
 		for(; i < len; i++){
 			var temp = data[i];
+			temp.w = temp.thumbWidth;
+			temp.h = temp.thumbHeight;
 			temp.area = temp.thumbWidth * temp.thumbHeight;
 			if(temp.originWidth > temp.originHeight * 1.15){
 				//横图
@@ -44,6 +49,7 @@ JJ.Arrange = function(){
 				//方图
 				pushSquareImage(temp);
 			}
+			pushImage(temp);
 			//
 			if(temp.area < smallArea){
 				smallArea = temp.area;
@@ -57,10 +63,24 @@ JJ.Arrange = function(){
 			}
 		}
 		
+		
+/******************************use packer.growing.js*********************************************/
+		var packer = new Packer(800, 430);
+		packer.fit(lists);
+//		console.log(lists[0], lists[1]);
+for(i = 0, len = lists.length; i < len; i++){
+	console.log(lists[i].fit);
+}		
+//		JJ.View.display(data, 0, lists.length);
+		
+		return;
+
+/***************************************************************************/
+		
 /*		console.log(horizontalImages);
 		console.log(verticalImages);
 		console.log(squareImages);
-		console.log(smallW, smallH, smallArea);*/
+		console.log(smallW, smallH, smallArea);
 		totalRow = Math.ceil(CON_HEIGHT / smallH);
 		totalCol = Math.ceil(CON_WIDTH / smallW);
 		baseCellW = CON_WIDTH / totalCol;
@@ -69,24 +89,51 @@ JJ.Arrange = function(){
 		for(var i = 0; i < totalRow; i++){
 			cellArr.push(new Array(totalCol));
 		}
-		
+/*		
 		//开始计算各个图片的位置
+		var p, first, end, matchArea, obj, areaW, areaH, 
 		for(i = 0; i < imageNum; i++){
+//			p = getNextPosition();
+//			first = p[0];
+//			end = p[p.length - 1];
+//			areaW = baseCellW * (end.left - first.left);
+//			areaH = baseCellH * (end.top - first.top);
+//			obj = getMatchObj(areaW, areaH);
+
+			obj = lists.shift();
+			p = getMatchArea(obj.thumbWidth, obj.thumbHeight);
 			
 		}
+*/
 	}
 	
-	//
+	//选择下一个最大的空白区域
 	function getNextPosition(){
-		var i = 0, temp, j, out = [];
+/*		var i = 0, temp, j, out = [];
 		for(i < totalRow; i++){
 			temp = cellArr[i];
 			for(j = 0; j < totalCol; j++){
 				if(!temp[j]){
-					out.push();
+					out.push({
+						left : j, 
+						top : i
+					});
+				}else if(out.length > 0){
+					return out;
 				}
 			}
 		}
+		return out;*/
+	}
+	
+	//
+	function getMatchObj(w, h){
+		return lists.shift();
+	}
+	
+	//
+	function getMatchArea(boxW, boxH){
+		
 	}
 	
 	//保存横图
@@ -127,6 +174,20 @@ JJ.Arrange = function(){
 			i++;
 		}
 		squareImages.push(obj);
+	}
+	
+	//
+	function pushImage(obj){
+		var i = 0, len = lists.length;
+		while(i < len){
+			var curr = lists[i];
+			if(curr.area <= obj.area){
+				lists.splice(i, 0, obj);
+				return;
+			}
+			i++;
+		}
+		lists.push(obj);
 	}
 	
 	//设置缩略图显示区域的宽高
--- src/js/display.js
+++ src/js/display.js
@@ -1,2 +1,28 @@
 //ÏÔÊ¾ËõÂÔÍ¼
 
+JJ.View = function(){
+	
+	var CON_ID = 'thumbContainer', 
+		TPL = '<div class="thumb-wrapper" style="left:#{left}px; top:#{top}px; width:#{boxWidth}px; height:#{boxHeight}px; "></div>';
+	
+	function display(data, start, end){
+		var elem = baidu.g(CON_ID), html = '', i, len = Math.min(end, data.length), temp, fit;
+		for(i = 0; i < len; i++){
+			temp = data[i];
+			fit = temp.fit;
+			temp.left = fit.x;
+			temp.top = fit.y;
+			temp.boxWidth = temp.thumbWidth - 2;
+			temp.boxHeight = temp.thumbHeight - 2;
+			
+			html += baidu.format(TPL, temp);
+		}
+		
+		elem.innerHTML = html;
+	}
+	
+	
+	return {
+		display : display
+	};
+}();
